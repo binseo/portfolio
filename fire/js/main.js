f7e2866517613668a6e8a81a21918ae5b7948145
binseo <oppfgh0901@naver.com>
2025-07-24 14:15:32 +0900

    .

--- fire/js/main.js
+++ fire/js/main.js
@@ -88,15 +88,90 @@ $(document).ready(function(){
 		$('.visual .ctrl_wrap button.btn_stop').show()
 	})
 		
-	
-	
+
 
 	/********************************** visual swiper :: 끝 ******************************/
 	
 
-	/********************************** story swiper :: 시작 ******************************/
 
-	/********************************** story swiper :: 끝 ******************************/
+
+	/********************************** notice swiper :: 시작 ******************************/
+
+	const notice_box_swiper = new Swiper('.notice_box .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			768: {    /* 640px 이상일때 적용 */
+				slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+			},
+			1024: {    /* 640px 이상일때 적용 */
+				slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 9,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		// loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+		// autoplay: {  /* 팝업 자동 실행 */
+		// 	delay: 2500,
+		// 	disableOnInteraction: true,
+		// },
+		navigation: {
+			nextEl: '.news .inner .notice .notice_box .tab_item .group .ctrl_wrap button.btn_next',
+			prevEl: '.news .inner .notice .notice_box .tab_item .group .ctrl_wrap button.btn_prev',
+		},
+	});	
+
+	/********************************** notice swiper :: 끝 ******************************/
+
+
+
+	/********************************** notice_popup swiper :: 시작 ******************************/
+	const notice_popup_swiper = new Swiper('.notice_popup .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		
+		slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            767: {    /* 640px 이상일때 적용 */
+                spaceBetween: 16,
+            },
+            768: {    /* 640px 이상일때 적용 */
+                spaceBetween: 24,
+            },
+
+        },
+
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+        navigation: {
+            nextEl: '.news .inner .notice_popup .ctrl_wrap button.btn_next',
+            prevEl: '.news .inner .notice_popup .ctrl_wrap button.btn_prev',
+        },
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.news .inner .notice_popup .ctrl_wrap .paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+    });
+
+    $('.news .inner .notice_popup .ctrl_wrap button.btn_stop').on('click', function(){
+        // console.log('정지버튼 클릭')
+        notice_popup_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide()
+        $('.news .inner .notice_popup .ctrl_wrap button.btn_play').show()
+    })
+
+    $('.news .inner .notice_popup .ctrl_wrap button.btn_play').on('click', function(){
+        // console.log('재생 버튼')
+        notice_popup_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide()
+        $('.news .inner .notice_popup .ctrl_wrap button.btn_stop').show()
+    })
+
+	/********************************** notice_popup swiper :: 끝 ******************************/
 
 
 	
